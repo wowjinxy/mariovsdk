05b51e620eb5cfb1b5b8473314494434c54c5122
camthehaxman <155208115+camthehaxman@users.noreply.github.com>
2024-06-28 09:42:54 -0500

    Match LZ compression for most levels (#7)
    * matching compression on one level file
    
    * level extraction
    
    * compress all levels that I can match
    
    * match compression on every level except tutorials
    
    * a few more tutorial levels match compression
    
    ---------
    
    Co-authored-by: camthehaxman <camthehaxman@users.noreply.github.com>

--- tools/gbagfx/lz.c
+++ tools/gbagfx/lz.c
@@ -1,9 +1,12 @@
 // Copyright (c) 2015 YamaArashi
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "global.h"
 #include "lz.h"
+
+#define DEBUG 0
 
 unsigned char *LZDecompress(unsigned char *src, int srcSize, int *uncompressedSize)
 {
@@ -11,6 +14,10 @@ unsigned char *LZDecompress(unsigned char *src, int srcSize, int *uncompressedSi
 		goto fail;
 
 	int destSize = (src[3] << 16) | (src[2] << 8) | src[1];
+
+#if DEBUG
+	printf("dest size = %i\n", destSize);
+#endif
 
 	unsigned char *dest = malloc(destSize);
 
@@ -45,14 +52,31 @@ unsigned char *LZDecompress(unsigned char *src, int srcSize, int *uncompressedSi
 				}
 
 				if (blockPos < 0)
+				{
+					printf("negative blockPos %i\n", blockPos);
+					printf("blockSize: %i\n", blockSize);
+					printf("blockDistance: %i\n", blockDistance);
 					goto fail;
-
+				}
+#if DEBUG
+				printf("copy %i bytes at 0x%X to 0x%X (distance %i): ", blockSize, blockPos, destPos, blockDistance);
+#endif
 				for (int j = 0; j < blockSize; j++)
+				{
+#if DEBUG
+					printf("%02X ", dest[blockPos + j]);
+#endif
 					dest[destPos++] = dest[blockPos + j];
+				}
+#if DEBUG
+				puts("");
+#endif
 			} else {
 				if (srcPos >= srcSize || destPos >= destSize)
 					goto fail;
-
+#if DEBUG
+				printf("byte %02X to 0x%X\n", src[srcPos], destPos);
+#endif
 				dest[destPos++] = src[srcPos++];
 			}
 
@@ -69,8 +93,12 @@ fail:
 	FATAL_ERROR("Fatal error while decompressing LZ file.\n");
 }
 
+#define MIN_BLOCK_SIZE 3
+
 unsigned char *LZCompress(unsigned char *src, int srcSize, int *compressedSize, const int minDistance)
 {
+	int prevBlockPos = 0;
+
 	if (srcSize <= 0)
 		goto fail;
 
@@ -101,7 +129,7 @@ unsigned char *LZCompress(unsigned char *src, int srcSize, int *compressedSize, 
 			int bestBlockDistance = 0;
 			int bestBlockSize = 0;
 			int blockDistance = minDistance;
-
+#if 0
 			while (blockDistance <= srcPos && blockDistance <= 0x1000) {
 				int blockStart = srcPos - blockDistance;
 				int blockSize = 0;
@@ -121,8 +149,66 @@ unsigned char *LZCompress(unsigned char *src, int srcSize, int *compressedSize, 
 
 				blockDistance++;
 			}
-
+#else
+			// starts looking from the previous blocks' position
+			(void)blockDistance;
+			int blockPos = prevBlockPos;
+			do
+			{
+				int blockSize = 0;
+				int test = blockPos;
+				while (blockSize < 18
+				 && srcPos + blockSize < srcSize  // don't go past end of file
+				 && src[test] == src[srcPos + blockSize]
+				 && blockSize <= srcPos /* seems to be the case? */)
+				{
+					blockSize++;
+					test++;
+					if (test >= srcPos)
+						test = blockPos;
+				}
+/*
+				printf("found length %i at 0x%X: ", blockSize, blockPos);
+				for (int i = 0; i < blockSize; i++)
+				{
+					printf("%02X ", src[blockPos + i]);
+				}
+				puts("");
+*/
+				if (blockSize > bestBlockSize)
+				{
+					bestBlockDistance = srcPos - blockPos;
+					bestBlockSize = blockSize;
+					if (blockSize == 18)
+						break;
+				}
+				
+				blockPos++;
+				// wrap around
+				if (blockPos >= srcPos)
+				{
+					blockPos = srcPos - 0x1000;
+					//puts("wrap");
+				}
+				if (blockPos < 0)
+				{
+					blockPos = 0;
+					//puts("zero");
+				}
+			} while (blockPos != prevBlockPos);
+			if (bestBlockSize >= MIN_BLOCK_SIZE) {
+				prevBlockPos = blockPos;
+			}
+			blockPos = srcPos - bestBlockDistance;
+#endif
+			assert(blockPos >= 0);
 			if (bestBlockSize >= 3) {
+#if DEBUG
+				printf("copy %i bytes at 0x%X to 0x%X: ", bestBlockSize, blockPos, destPos - 5);
+				for (int j = 0; j < bestBlockSize; j++)
+					printf("%02X ", src[blockPos + j]);
+				puts("");
+#endif
 				*flags |= (0x80 >> i);
 				srcPos += bestBlockSize;
 				bestBlockSize -= 3;
@@ -130,6 +216,9 @@ unsigned char *LZCompress(unsigned char *src, int srcSize, int *compressedSize, 
 				dest[destPos++] = (bestBlockSize << 4) | ((unsigned int)bestBlockDistance >> 8);
 				dest[destPos++] = (unsigned char)bestBlockDistance;
 			} else {
+#if DEBUG
+				printf("byte %02X to 0x%X\n", src[srcPos], destPos - 1 - 4 /*skip header*/);
+#endif
 				dest[destPos++] = src[srcPos++];
 			}
 
